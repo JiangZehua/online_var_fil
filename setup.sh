42269473506b91bebced38d3dc733ddc3aa65981
Zehua Jiang <jiangzehua_work@foxmail.com>
2022-04-27 22:48:28 -0400

    add git ignore and setup, add trained vae models

---
+++ setup.sh
@@ -0,0 +1,6 @@
+python -m pip install tqdm scipy pandas matplotlib
+
+conda install pytorch torchvision cudatoolkit=10.2 -c pytorch
+# conda install pytorch torchvision cudatoolkit=11.3 -c pytorch
+
+python -m pip install hydra-core --upgrade
